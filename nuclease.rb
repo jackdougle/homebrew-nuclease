da8941d2c8fdf1f931106022c6cc81f2786045b4
Jack Douglass <102892299+jackdougle@users.noreply.github.com>
2025-09-23 12:17:10 -0700

    Create nuclease.rb

---
+++ nuclease.rb
@@ -0,0 +1,18 @@
+class Nuclease < Formula
+  desc "Fast Rust DNA read filtering tool inspired by BBDuk"
+  homepage "https://github.com/jackdougle/nuclease"
+  url "https://github.com/jackdougle/nuclease/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "sha256:6cb85ee475aaf565b0d52791e488b8d373b9ad950d1e4f2c184c8661eeb79843"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    # Simple version check to ensure it was installed properly
+    assert_match "nuclease", shell_output("#{bin}/nuclease --version")
+  end
+end
