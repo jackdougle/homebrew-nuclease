985f5b941210de814085e0fdbe167cbb788aa1f2
Jack Douglass <102892299+jackdougle@users.noreply.github.com>
2025-09-23 22:26:16 -0700

    Update nuclease.rb

--- nuclease.rb
+++ nuclease.rb
@@ -2,7 +2,7 @@ class Nuclease < Formula
   desc "Fast Rust DNA read filtering tool inspired by BBDuk"
   homepage "https://github.com/jackdougle/nuclease"
   url "https://github.com/jackdougle/nuclease/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "sha256:6cb85ee475aaf565b0d52791e488b8d373b9ad950d1e4f2c184c8661eeb79843"
+  sha256 "6cb85ee475aaf565b0d52791e488b8d373b9ad950d1e4f2c184c8661eeb79843"
   license "MIT"
 
   depends_on "rust" => :build
